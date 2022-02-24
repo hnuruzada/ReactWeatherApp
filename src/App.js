697be3b60d6d082bb77a9ceacf052eecfcf86153
hnuruzada <90691865+hnuruzada@users.noreply.github.com>
2022-02-25 03:28:26 +0400

    created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import {Weather} from "./components/Weather";
+
+export const App=()=> {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+
